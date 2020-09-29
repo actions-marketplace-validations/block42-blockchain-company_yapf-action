8ca19d2c9bdbfacac643f7943b8be477823b0107
Paul to the Plessing <paul.plessing@block42.tech>
2020-09-29 14:44:47 +0700

    removed pep8 specification which is default anyway

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 echo "Formatting $GITHUB_REPOSITORY"
-sh -c "yapf --in-place --recursive --style='{based_on_style: pep8}' $GITHUB_WORKSPACE $*"
+sh -c "yapf --in-place --recursive $GITHUB_WORKSPACE $*"
